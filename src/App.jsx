a34d2009f4cc1fd178c12ad61e70db148eeb4392
Kimiqo <michael12darko@gmail.com>
2025-04-11 14:24:12 +0000

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
-              {/* Vercel Speed Insights */}
+              {/* Vercel Speed Insights and Analytics*/}
               <SpeedInsights />
+              <Analytics/>
     </Router>
   );
 }
